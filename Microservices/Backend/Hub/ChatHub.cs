757b0b614f621daae2d5d7e0ac1e69907b9798e1
Sergey <sergeyvolkovink@gmail.com>
2023-06-17 21:57:57 +0300

    added first hub

---
+++ Microservices/Backend/Hub/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Backend.Hab
+{
+    public class ChatHub: Hub
+    {
+        public async Task Send(string message)
+        {
+
+            /*
+             * message - какойто контейнер из фронта содержащий токен  и тело сообщения
+            1) надо проверить токен и записать сообщение в базы
+            2) обаработать список Clients?
+                создавать группы? - Hub.Groups 
+
+            https://learn.microsoft.com/ru-ru/aspnet/signalr/overview/guide-to-the-api/working-with-groups
+            https://learn.microsoft.com/ru-ru/aspnet/signalr/overview/guide-to-the-api/mapping-users-to-connections
+             * 
+             */
+
+            await this.Clients.All.SendAsync("Receive", message);
+
+            
+        }
+    }
+}
