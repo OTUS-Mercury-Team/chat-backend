98776198d797aa4cf23f6cae106358d0e136d9c1
Sergey Volkov <sergeyvolkovink@gmail.com>
2023-08-05 20:07:03 +0300

    add test UI by Maks

--- Microservices/Backend/Program.cs
+++ Microservices/Backend/Program.cs
@@ -1,18 +1,32 @@
 using Backend.db;
 using Microsoft.EntityFrameworkCore;
+using SignalRChat.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://example.com")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .AllowCredentials();
+        });
+});
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,11 +34,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
